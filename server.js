de9a540c7478708a855b5e4ca32488e897be1680
AlpenidzeMartha <alpenidzemartha1@gmail.com>
2022-02-17 23:08:10 +0400

    static middleware

--- server.js
+++ server.js
@@ -9,12 +9,31 @@ var fs =require ("fs");
 //parse the request parameters
 app.use(express.json());
 
+
+//serve the static files
+app.use(express.static('public'));
+
+
 app.use ((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers","*");
     res.setHeader('Access-Control-Allow-Origin', 'PUT');
     next();
 })
+
+//static middleware                             
+app.use(function(req, res, next) {
+    var filePath = path.join(__dirname,  "images", req.url);
+    fs.stat(filePath, function(err, fileInfo){
+        if (err) {
+            next();
+            return;
+            }
+            if (fileInfo.isFile()) res.sendFile(filePath);
+            else next();
+            
+        });
+    });
 
 
 const port = process.env.PORT || 3000;
